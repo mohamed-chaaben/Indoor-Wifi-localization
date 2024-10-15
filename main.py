c37d4943672edf5e01ae4fce687114c47095f5bc
Mohamed Chaaben <69085814+mohamed-chaaben@users.noreply.github.com>
2024-10-15 14:36:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,21 +19,22 @@ for subgroup in subgroups:
         data = Data(path='all_dataframeV2.csv', features=features, subgroup=subgroup, drop_duplicates=False)
         print(len(data.X))
         min_points = 6
-        eps_estimation = EpsEstimation(data.X, min_points)
+        eps_estimation = EpsEstimation(data.X, min_points, metric='precomputed')
         eps = eps_estimation.fit()
         eps_estimation.plot()
         print(eps)
-        # mlflow.log_param('subgroup', subgroup)
-        # mlflow.log_param('min_points', min_points)
-        # mlflow.log_param('eps', eps)
-        # cls = Clustering(data.X, data.labels, eps, min_points, metric='precomputed')
-        # cls.fit()
+        print(eps_estimation.knees)
+        mlflow.log_param('subgroup', subgroup)
+        mlflow.log_param('min_points', min_points)
+        mlflow.log_param('eps', eps)
+        cls = Clustering(data.X, data.labels, eps, min_points, metric='precomputed')
+        cls.fit()
 
-        # mlflow.log_metric('homogeneity', cls.homogeneity)
-        # mlflow.log_metric('completeness', cls.completeness)
-        # mlflow.log_metric('v_measure', cls.v_measure)
-        # mlflow.log_metric('avg_silhouette', cls.avg_silhouette)
-        # cls.plot()
+        mlflow.log_metric('homogeneity', cls.homogeneity)
+        mlflow.log_metric('completeness', cls.completeness)
+        mlflow.log_metric('v_measure', cls.v_measure)
+        mlflow.log_metric('avg_silhouette', cls.avg_silhouette)
+        cls.plot()
     
 
 
